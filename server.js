a29db0c538cb09032c586e8a97a2b14f820e780e
Neha Anand <neha280103@gmail.com>
2022-02-28 21:56:34 +0530

    added post method

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-
+//function 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/bmiCalculator.html");
-})
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
 
-app.listen(port,() => {
-    console.log("Server started on port " + port);
+//function calculates the bmi
+function bmiCalculator(weight, height) {
+  var bmi = Math.round(weight / Math.pow(height, 2));
+  if (bmi < 18.5) {
+    interpretation = "Your BMI is " + bmi + ", so you are underweight.";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    interpretation = "Your BMI is " + bmi + ", so you are normal weight.";
+  } else {
+    interpretation = "Your BMI is " + bmi + ", so you are over weight.";
+  }
+  return interpretation;
+}
+
+app.post("/", (req, res) => {
+  var wt = Number(req.body.weight);
+  var ht = Number(req.body.height);
+  var interpretation = bmiCalculator(wt, ht);
+  res.send(interpretation);
+});
+
+
+app.listen(port, () => {
+  console.log("Server started on port " + port);
 });
